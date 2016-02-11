013ffe4928db70ab142dd34eb38ca4799be9810c
Donald Atkinson <fuzzicallogic@outlook.com>
2016-02-11 12:10:22 -0700

    Prep for proxying requests

--- src/servicerouter.js
+++ src/servicerouter.js
@@ -2,13 +2,28 @@ module.exports = (function() {
 	var services = require('./services.js');
 
 	return function(request, response, next) {
-		var serviceName = request.url.replace('/', '');
+		var paths = request.url.split('/'),
+			serviceName = paths[1];
 		var service = services.find(function(v, k, a) {
 			if (v.name === serviceName)
 				return v;
 		}, services);
-		response.end('Service found!');
+
+
+
 		if (!!!service)
 			next();
+		else {
+			console.log('Service: ' + service.name);
+			var proxyUrl = request.url.replace('/' + service.name, '');
+			console.log('New Request URL: ' + proxyUrl);
+			proxyRequest('http://localhost:' + service.port + proxyUrl);
+		}
+
+		response.end('Service found!');
 	};
+
+	function proxyRequest(url) {
+		console.log('Proxy Request to: ' + url);
+	}
 }) ();
